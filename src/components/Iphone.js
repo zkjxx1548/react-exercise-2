d4bf3eaaa4d35fa01895ec042929971e56be5011
zkj <438101845@qq.com>
2020-08-12 00:21:45 +0800

    [zkj]-{feat}: add Iphone by real data

--- src/components/Iphone.js
+++ src/components/Iphone.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import IphoneItem from './IphoneItem.js';
+//import Product from './Product.js';
+import fetchData from '../async/myfetch.js';
 
 class Iphone extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = ({
+      products: []
+    });
+    fetchData().then(res => {
+      this.setState({
+        products: res
+      });
+    });
+  }
 
   render() {
-    return (
-      <div>
-        <IphoneItem />
-        <IphoneItem />
-        <IphoneItem />
-      </div>
-    );
+    //const iphoneArray = this.state.products.filter(product => product.category === 'iPhone');
+    return this.state.products.map(element => (<div>
+      <h3>{element.name}</h3>
+      <img src="../assets/product_image_placeholder.png" />
+      <span>{element.price}</span>
+      <button>add to cart</button>
+    </div>));
   }
 }
 export default Iphone;
